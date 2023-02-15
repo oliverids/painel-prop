abc9168b632e2ba8f419f3134a0633f81934d38c
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-02-15 18:46:09 -0300

    adding cities

--- public/js/components/map.js
+++ public/js/components/map.js
@@ -4,7 +4,7 @@ const dados = await fetch('./dados.json').then(r => r.json());
 let popupContent;
 
 const map = L.map('map', {
-    center: [-20, -40],
+    center: [-20, -40.5],
     zoom: 7,
     minZoom: 7,
     maxZoom: 10,
@@ -14,6 +14,27 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 12,
 }).addTo(map);
 
+const main = document.querySelector('main'),
+    infoSection = document.querySelector('.info');
+
+infoSection.addEventListener('scroll', () => {
+    // if (infoSection.scrollTop !== 0) {
+        main.classList.add('scrolldown')
+    // } 
+    // else if(infoSection.clientHeight < infoSection.scrollHeight || infoSection.clientHeight == infoSection.scrollHeight) {
+    //     main.classList.add('scrolldown')
+
+    // }
+    
+    // else {
+    //     main.classList.remove('scrolldown')
+    // }
+})
+
+map.on('click', () => {
+    main.classList.remove('scrolldown')
+})
+
 let allMarkers = [];
 for (let i = 0; i < dados.length; i++) {
     popupContent = /*html*/`<p>${dados[i].cidade.nome}</p>`;
@@ -22,17 +43,47 @@ for (let i = 0; i < dados.length; i++) {
     allMarkers.push(marker)
 }
 
-const main = document.querySelector('main'),
-    infoSection = document.querySelector('.info');
+//cities
 
-infoSection.addEventListener('scroll', () => {
-    if (infoSection.scrollTop !== 0) {
-        main.classList.add('scrolldown')
-    } else {
-        main.classList.remove('scrolldown')
+const visibleList = document.getElementById('visibleList');
+function pullMapCities() {
+    let bounds = map.getBounds();
+    visibleList.innerHTML = '';
+
+    let visibleMarkers = [];
+
+    allMarkers.forEach(each => {
+        let isInBounds = bounds.contains(each._latlng)
+
+        if (isInBounds) {
+            let visibleCity = document.createElement('li');
+            visibleMarkers.push(each);
+
+            let itemVisivel = dados.find(item =>
+                item.cidade.lat === each._latlng.lat &&
+                item.cidade.lon === each._latlng.lng);
+
+            visibleCity.innerHTML = /*html*/`
+                <div></div>
+                <a>${itemVisivel.cidade.nome}</a>
+            `;
+
+            visibleList.appendChild(visibleCity);
+        }
+    })
+
+    if (!visibleMarkers.length) {
+        for (let i = 0; i < dados.length; i++) {
+            let visibleCity = document.createElement('li');
+
+            visibleCity.innerHTML = /*html*/`
+                <div></div>
+                <a>${dados[i].cidade.nome}</a>
+            `;
+
+            visibleList.appendChild(visibleCity);
+        }
     }
-})
+}
 
-map.on('click', () => {
-    main.classList.remove('scrolldown')
-})
+pullMapCities()
